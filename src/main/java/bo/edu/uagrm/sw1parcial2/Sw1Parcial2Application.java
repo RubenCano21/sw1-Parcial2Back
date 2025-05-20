38e5ba1223020ddb2c688fe7c83946578e541da2
RubenCano21 <ruben217.rc@gmail.com>
2025-05-20 11:20:24 -0400

    first commit

---
+++ src/main/java/bo/edu/uagrm/sw1parcial2/Sw1Parcial2Application.java
@@ -0,0 +1,13 @@
+package bo.edu.uagrm.sw1parcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sw1Parcial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sw1Parcial2Application.class, args);
+    }
+
+}
---
+++ src/test/java/bo/edu/uagrm/sw1parcial2/Sw1Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.uagrm.sw1parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sw1Parcial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
