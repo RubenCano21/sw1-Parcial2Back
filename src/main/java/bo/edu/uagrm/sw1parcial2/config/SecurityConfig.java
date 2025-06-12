b344671b25d2db6eabbd167a9d3c7822e91b1a05
Nandylui <luisbuceta060@gmaiil.com>
2025-06-12 17:20:21 -0400

    conectando el flutter

--- src/main/java/bo/edu/uagrm/sw1parcial2/config/SecurityConfig.java
+++ src/main/java/bo/edu/uagrm/sw1parcial2/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173")); // o usa setAllowedOrigins si prefieres
+        config.setAllowedOriginPatterns(List.of("https://flutter-ui-afaz.onrender.com")); // o usa setAllowedOrigins si prefieres
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setAllowCredentials(true);
